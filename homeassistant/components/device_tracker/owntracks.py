2c80c59e681155937b768ededab71e5ca5b5ffd0
Fabian Affolter <fabian@affolter-engineering.ch>
2017-10-09 14:24:22 +0200

    Update exception

--- homeassistant/components/device_tracker/owntracks.py
+++ homeassistant/components/device_tracker/owntracks.py
@@ -5,22 +5,21 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/device_tracker.owntracks/
 """
 import asyncio
+import base64
 import json
 import logging
-import base64
 from collections import defaultdict
 
 import voluptuous as vol
 
-from homeassistant.core import callback
-import homeassistant.helpers.config_validation as cv
 import homeassistant.components.mqtt as mqtt
-from homeassistant.const import STATE_HOME
-from homeassistant.util import slugify, decorator
+import homeassistant.helpers.config_validation as cv
 from homeassistant.components import zone as zone_comp
 from homeassistant.components.device_tracker import PLATFORM_SCHEMA
+from homeassistant.const import STATE_HOME
+from homeassistant.core import callback
+from homeassistant.util import slugify, decorator
 
-DEPENDENCIES = ['mqtt']
 REQUIREMENTS = ['libnacl==1.6.0']
 
 _LOGGER = logging.getLogger(__name__)
@@ -33,6 +32,8 @@ CONF_MAX_GPS_ACCURACY = 'max_gps_accuracy'
 CONF_SECRET = 'secret'
 CONF_WAYPOINT_IMPORT = 'waypoints'
 CONF_WAYPOINT_WHITELIST = 'waypoint_whitelist'
+
+DEPENDENCIES = ['mqtt']
 
 OWNTRACKS_TOPIC = 'owntracks/#'
 
@@ -138,6 +139,8 @@ def _set_gps_from_zone(kwargs, location, zone):
 
 def _decrypt_payload(secret, topic, ciphertext):
     """Decrypt encrypted payload."""
+    from libnacl import CryptError
+
     try:
         keylen, decrypt = get_cipher()
     except OSError:
@@ -166,7 +169,7 @@ def _decrypt_payload(secret, topic, ciphertext):
         message = message.decode("utf-8")
         _LOGGER.debug("Decrypted payload: %s", message)
         return message
-    except ValueError:
+    except CryptError:
         _LOGGER.warning(
             "Ignoring encrypted payload because unable to decrypt using "
             "key for topic %s", topic)
